7116332602703b9b73a98c4692bab3088ed2c607
Alec Hance <alecnhance@gmail.com>
2025-07-27 20:36:05 -0400

    initial commit

---
+++ WorkoutLog/WorkoutLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLogApp.swift
+//  WorkoutLog
+//
+//  Created by Alec Hance on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutLogTests/WorkoutLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLogTests.swift
+//  WorkoutLogTests
+//
+//  Created by Alec Hance on 7/27/25.
+//
+
+import Testing
+@testable import WorkoutLog
+
+struct WorkoutLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
