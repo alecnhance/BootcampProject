b81998480a69f5d7186372c566faadf804f42ed4
Alec Hance <alecnhance@gmail.com>
2025-08-18 07:27:58 -0400

    muscle selection binding

---
+++ WorkoutLog/Views/MultiSelect.swift
@@ -0,0 +1,18 @@
+//
+//  MultiSelect.swift
+//  WorkoutLog
+//
+//  Created by Alec Hance on 8/17/25.
+//
+
+import SwiftUI
+
+struct MultiSelect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiSelect()
+}
